01d0d908e35d9357d3e055b9425e94a5f3970aa1
ewhipp <whipp.erik@gmail.com>
2018-08-09 20:02:59 -0700

    CLEAN UP

--- init.lua
+++ init.lua
@@ -1,57 +1,30 @@
 -- Hammerspoon configuration by Erik Whipp
 
----------Constants--------------------------------
+--------- COMBINATIONS -----------------
 
 local cmd_shift = {"cmd", "shift"}
+local cmd_ctrl = {"cmd", "ctrl"}
+local cmd_alt_ctrl = {"cmd", "alt", "ctrl"};
+
+--------- MONITORS -----------------
+
 local primary_monitor= "Color LCD"
 local second_monitor = "SyncMaster"
 local third_monitor = "DELL S2330MX"
-local mainBrowser = "Safari"
-local mainEditor = "xCode"
+local work_monitor = "HD 709-A"
+
+--------- PROGRAMS -----------------
+
+local mainBrowser = "Google Chrome"
+local mainEditor = "IntelliJ IDEA"
 local ipadMonitor = "Display"
 local mainTerm = "Terminal"
+local slack = "Slack"
+local m_Control = "MoneyControl"
 
---------------------------------------------------
--- cmd + shift + l move windowSize right + 50 pixels
-hs.hotkey.bind({"cmd", "shift"}, "h", function()
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-
-  f.x = f.x - 50
-  win:setFrame(f)
-end)
-
--- cmd + shift + h resize window left + 50 pixels
-hs.hotkey.bind({"cmd", "shift"}, "l", function()
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-
-  f.x = f.x + 50
-  win:setFrame(f)
-end)
-
--- cmd shift + k resize window upward + 50 pixels
-hs.hotkey.bind({"cmd", "shift"}, "k", function()
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-
-  f.x = f.y + 50
-  win:setFrame(f)
-end)
-
--- cmd shift + j resize window upward + 50 pixels
-hs.hotkey.bind({"cmd", "shift"}, "j", function()
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-
-  f.x = f.y - 50
-  win:setFrame(f)
-end)
-
-
--- Tile two findows next to one another
--- first is left, second is right
-hs.hotkey.bind({"cmd", "ctrl"}, "Left", function()
+--------- SCREEN BLOCKS ------------
+-- Left
+hs.hotkey.bind(cmd_ctrl, "Left", function()
   local win = hs.window.focusedWindow()
   local f = win:frame()
   local screen = win:screen()
@@ -65,7 +38,7 @@ hs.hotkey.bind({"cmd", "ctrl"}, "Left", function()
   end)
 
   -- Right
-  hs.hotkey.bind({"cmd", "ctrl"}, "Right", function()
+  hs.hotkey.bind(cmd_ctrl, "Right", function()
   local win = hs.window.focusedWindow()
   local f = win:frame()
   local screen = win:screen()
@@ -79,7 +52,7 @@ hs.hotkey.bind({"cmd", "ctrl"}, "Left", function()
   end)
 
   -- Full Screen
-  hs.hotkey.bind({"cmd", "ctrl"}, "Up", function()
+  hs.hotkey.bind(cmd_ctrl, "Up", function()
   local win = hs.window.focusedWindow()
   local f = win:frame()
   local screen = win:screen()
@@ -93,7 +66,7 @@ hs.hotkey.bind({"cmd", "ctrl"}, "Left", function()
   end)
 
   -- move to mid screen
-  hs.hotkey.bind({"cmd", "ctrl"}, "Down", function()
+  hs.hotkey.bind(cmd_ctrl, "Down", function()
   local win = hs.window.focusedWindow()
   local f = win:frame()
   local screen = win:screen()
@@ -106,9 +79,8 @@ hs.hotkey.bind({"cmd", "ctrl"}, "Left", function()
   win:setFrame(f)
 end)
 
--- Move between monitors
+--------- RELOAD CONFIG AUTO ------------
 
-  -- reload the configuration automatically when file changes
 function reloadConfig(files)
   reloadCompleted = false
   for _, file in pairs(files) do
@@ -120,16 +92,35 @@ function reloadConfig(files)
     hs.reload()
   end
 end
+
+-- DO IT --
 local myWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
 hs.alert.show("Success")
 
-  -- Launch various applications
-hs.hotkey.bind({"cmd"}, "W", function()
-  hs.application.launchOrFocus(mainBrowser)
+
+--------- APPLICATION COMMANDS ---------
+hs.hotkey.bind(cmd_shift, "T", function()
+  hs.application.launchOrFocus(mainTerm)
 end)
 
-hs.hotkey.bind(cmd_shift, "T", function()
-  hs.application.launchOrFocus("mainTerm")
+hs.hotkey.bind(cmd_shift, "B", function()
+  hs.application.launchOrFocus(slack)
+end)
+
+hs.hotkey.bind(cmd_shift, "M", function()
+  hs.application.launchOrFocus("Spotify")
+end)
+
+hs.hotkey.bind(cmd_shift, "C", function()
+  hs.application.launchOrFocus("Messages")
+end)
+
+hs.hotkey.bind(cmd_shift, "X", function()
+  hs.application.launchOrFocus("xCode")
+end)
+
+hs.hotkey.bind({"cmd"}, "W", function()
+  hs.application.launchOrFocus(mainBrowser)
 end)
 
 hs.hotkey.bind({"cmd"}, "M", function()
@@ -137,41 +128,26 @@ hs.hotkey.bind({"cmd"}, "M", function()
 end)
 
 hs.hotkey.bind({"cmd"}, "E", function()
-  hs.application.launchOrFocus("Atom")
+  hs.application.launchOrFocus(mainEditor)
 end)
 
-hs.hotkey.bind({"cmd", "shift"}, "M", function()
-  hs.application.launchOrFocus("Spotify")
-end)
+--------- CONTROL SPOTIFY ---------
 
-hs.hotkey.bind({"cmd", "shift"}, "B", function()
-  hs.application.launchOrFocus("qutebrowser")
-end)
-
-hs.hotkey.bind({"cmd", "shift"}, "C", function()
-  hs.application.launchOrFocus("Messages")
-end)
-
-hs.hotkey.bind({"cmd", "shift"}, "X", function()
-  hs.application.launchOrFocus("xCode")
-end)
-
--- automatically play spotify
-hs.hotkey.bind({"cmd", "ctrl"}, "P", function()
+hs.hotkey.bind(cmd_ctrl, "P", function()
   hs.spotify.play()
   hs.spotify.displayCurrentTrack()
 end)
 
--- stop spotify
 hs.hotkey.bind({"cmd", "ctrl"}, "S", function()
   hs.spotify.pause()
 end)
 
---------------------BEGIN POMORODO CLOCK-------------------------------
+-------- BEGIN POMORODO CLOCK ---------
+
 local pom={}
+
 pom.bar = {
-  indicator_height = 0.2, -- ratio from the height of the menubar (0..1)
-  indicator_alpha  = 0.3,
+  indicator_height = 0.2,
   indicator_in_all_spaces = true,
   color_time_remaining = hs.drawing.color.green,
   color_time_used      = hs.drawing.color.red,
@@ -184,7 +160,6 @@ pom.config = {
   enable_color_bar = true,
   work_period_sec  = 25 * 60,
   rest_period_sec  = 5 * 60,
-
 }
 
 pom.var = {
@@ -196,32 +171,31 @@ pom.var = {
   max_time_sec     = pom.config.work_period_sec
 }
 
---------------------------------------------------------------------------------
--- Color bar for pomodoor
---------------------------------------------------------------------------------
+--------- COLOR BAR FOR POMODORO ---------
 
 function pom_del_indicators()
   pom.bar.c_left:delete()
   pom.bar.c_used:delete()
 end
 
-function pom_draw_on_menu(target_draw, screen, offset, width, fill_color)
+function pom_draw_time_left_on_menu(target_draw, screen, offset, width, fill_color)
   local screeng                  = screen:fullFrame()
   local screen_frame_height      = screen:frame().y
   local screen_full_frame_height = screeng.y
   local height_delta             = screen_frame_height - screen_full_frame_height
   local height                   = pom.bar.indicator_height * (height_delta)
 
+  -- AS THE PROGRESSES
   target_draw:setSize(hs.geometry.rect(screeng.x + offset, screen_full_frame_height, width, height))
   target_draw:setTopLeft(hs.geometry.point(screeng.x + offset, screen_full_frame_height))
   target_draw:setFillColor(fill_color)
   target_draw:setFill(true)
-  target_draw:setAlpha(pom.bar.indicator_alpha)
   target_draw:setLevel(hs.drawing.windowLevels.overlay)
   target_draw:setStroke(false)
   if pom.bar.indicator_in_all_spaces then
     target_draw:setBehavior(hs.drawing.windowBehaviors.canJoinAllSpaces)
   end
+
   target_draw:show()
 end
 
@@ -232,26 +206,18 @@ function pom_draw_indicator(time_left, max_time)
   local width       = math.ceil(screeng.w * time_ratio)
   local left_width  = screeng.w - width
 
-  pom_draw_on_menu(pom.bar.c_left, main_screen, left_width, width,      pom.bar.color_time_remaining)
-  pom_draw_on_menu(pom.bar.c_used, main_screen, 0,          left_width, pom.bar.color_time_used)
-
+  pom_draw_time_left_on_menu(pom.bar.c_left, main_screen, left_width, width, pom.bar.color_time_remaining)
+  pom_draw_time_left_on_menu(pom.bar.c_used, main_screen, 0, left_width, pom.bar.color_time_used)
 end
---------------------------------------------------------------------------------
 
--- update display
-local function pom_update_display()
+local function pom_time_display()
   local time_min = math.floor( (pom.var.time_left / 60))
   local time_sec = pom.var.time_left - (time_min * 60)
   local str = string.format ("[%s|%02d:%02d|#%02d]", pom.var.curr_active_type, time_min, time_sec, pom.var.work_count)
   pom_menu:setTitle(str)
 end
 
--- stop the clock
--- Stateful:
--- * Disabling once will pause the countdown
--- * Disabling twice will reset the countdown
--- * Disabling trice will shut down and hide the pomodoro timer
-function pom_disable()
+function disable_pom_clock()
 
   local pom_was_active = pom.var.is_active
   pom.var.is_active = false
@@ -260,10 +226,12 @@ function pom_disable()
      if (pom_was_active) then
       pom_timer:stop()
     end
+
   elseif (pom.var.disable_count == 1) then
     pom.var.time_left         = pom.config.work_period_sec
     pom.var.curr_active_type  = "work"
-    pom_update_display()
+    pom_update_time()
+
   elseif (pom.var.disable_count >= 2) then
     if pom_menu == nil then
       pom.var.disable_count = 2
@@ -278,10 +246,8 @@ function pom_disable()
   end
 
   pom.var.disable_count = pom.var.disable_count + 1
-
 end
 
--- update pomodoro timer
 local function pom_update_time()
   if pom.var.is_active == false then
     return
@@ -304,7 +270,6 @@ local function pom_update_time()
       end
     end
 
-    -- draw color bar indicator, if enabled.
     if (pom.config.enable_color_bar == true) then
       pom_draw_indicator(pom.var.time_left, pom.var.max_time_sec)
     end
@@ -312,10 +277,9 @@ local function pom_update_time()
   end
 end
 
--- update menu display
-local function pom_update_menu()
+local function pom_timer()
   pom_update_time()
-  pom_update_display()
+  pom_time_display()
 end
 
 local function pom_create_menu(pom_origin)
@@ -326,7 +290,6 @@ local function pom_create_menu(pom_origin)
   end
 end
 
--- start the pomodoro timer
 function pom_enable()
   pom.var.disable_count = 0;
   if (pom.var.is_active) then
@@ -340,71 +303,28 @@ function pom_enable()
   pom_timer:start()
 end
 
--- reset work count
--- TODO - reset automatically every day
-function pom_reset_work()
+function pom_reset_timer()
   pom.var.work_count = 0;
 end
 
 hs.hotkey.bind("cmd", '1', function() pom_enable() end)
 hs.hotkey.bind("cmd", '0', function() pom_disable() end)
----------------------------END POM CLOCK--------------------------------------
 
--- focus windows
-hs.hotkey.bind("cmd", "L", hs.window.focusWindowEast)
-hs.hotkey.bind("cmd", "H", hs.window.focusWindowWest)
-hs.hotkey.bind("cmd", "K", hs.window.focusWindowNorth)
-hs.hotkey.bind("cmd", "J", hs.window.focusWindowSouth)
+--------- END POMODORO CLOCK --------
 
--- move windows to different workspace
--- Switch windows to next space at left or right
-function moveWindowOneSpace(direction)
-  _mouseOrigin = hs.mouse.getAbsolutePosition()
-  local win = hs.window.focusedWindow()
-  _clickPoint = win:zoomButtonRect()
+--------- OPEN WEBSITES -------------
 
-  _clickPoint.x = _clickPoint.x + _clickPoint.w + 5
-  _clickPoint.y = _clickPoint.y + (_clickPoint.h / 2)
-
-  local mouseClickEvent = hs.eventtap.event.newMouseEvent(hs.eventtap.event.types.leftMouseDown, _clickPoint)
-  mouseClickEvent:post()
-
-  hs.timer.usleep(100000)
-
-  local nextSpaceDownEvent = hs.eventtap.event.newKeyEvent({"ctrl"}, direction, true)
-  nextSpaceDownEvent:post()
-end
-
-function moveWindowOneSpaceEnd(direction)
-  local nextSpaceUpEvent = hs.eventtap.event.newKeyEvent({"ctrl"}, direction, false)
-  nextSpaceUpEvent:post()
-  hs.timer.usleep(100000)
-  local mouseReleaseEvent = hs.eventtap.event.newMouseEvent(hs.eventtap.event.types.leftMouseUp, _clickPoint)
-  mouseReleaseEvent:post()
-  hs.timer.usleep(100000)
-  hs.mouse.setAbsolutePosition(_mouseOrigin)
-end
-
--- hotkey for moving windows
-hk1 = hs.hotkey.bind({"cmd","shift"}, "right",
-  function() moveWindowOneSpace("right")
-end)
-hk2 = hs.hotkey.bind({"cmd","shift"}, "left",
-  function() moveWindowOneSpace("left")
-end)
-
--- Open google drive from hotkey
 function openGoogleDrive()
   local googleDrive = "https://drive.google.com/drive/u/0/my-drive"
   hs.execute("open " .. googleDrive)
   hs.application.launchOrFocus(mainBrowser)
 end
 
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "W", function() openGoogleDrive()
-end)
+hs.hotkey.bind(cmd_alt_ctrl, "W", function() openGoogleDrive() end)
 
--- move to different monitors
-hs.hotkey.bind({"cmd", "shift"}, "1", function()
+--------- FOCUS MONITOR -------------
+
+hs.hotkey.bind(cmd_shift, "1", function()
   local win = hs.window.focusedWindow()
   if (win) then
     win:moveToScreen(hs.screen.get(primary_monitor))
@@ -415,15 +335,7 @@ end)
 hs.hotkey.bind( cmd_shift, "2", function()
   local win = hs.window.focusedWindow()
   if (win) then
-    win:moveToScreen(hs.screen.get(second_monitor))
-    win:maximize()
-  end
-end)
-
-hs.hotkey.bind( cmd_shift, "3", function()
-  local win = hs.window.focusedWindow()
-  if (win) then
-    win:moveToScreen(hs.screen.get(third_monitor))
+    win:moveToScreen(hs.screen.get(work_monitor))
     win:maximize()
   end
 end)
