69e5147a441487be70ac0d6ba5780bbfc8e80c2d
Erik Whipp <whipp.erik@gmail.com>
2018-10-07 13:09:21 -0700

    Add corners

--- init.lua
+++ init.lua
@@ -50,13 +50,66 @@ hs.hotkey.bind(cmd_ctrl, "Left", function()
   local f = win:frame()
   local screen = win:screen()
   local max = screen:frame()
-
   f.x = max.x + (max.w / 2)
   f.y = max.y
   f.w = max.w / 2
   f.h = max.h
   win:setFrame(f)
   end)
+  
+  -- Corners of the Screen
+  -- Top Right
+  hs.hotkey.bind({ "cmd", "ctrl"}, "]", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+  f.x = max.x + (max.w / 2)
+  f.y = max.y 
+  f.w = max.w 
+  f.h = max.h / 2
+  win:setFrame(f)
+  end)
+
+  -- Bottom Right
+  hs.hotkey.bind({ "cmd", "ctrl"}, "'", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+  f.x = max.x + (max.w / 2)
+  f.y = (max.h / 2)
+  f.w = max.w 
+  f.h = max.h / 2
+  win:setFrame(f)
+  end)
+
+  -- Top left
+  hs.hotkey.bind({ "cmd", "ctrl"}, "[", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+  f.x = max.x 
+  f.y = max.y 
+  f.w = max.w / 2
+  f.h = max.h / 2
+  win:setFrame(f)
+  end)
+
+  -- Bottom left
+  hs.hotkey.bind({ "cmd", "ctrl"}, ";", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+  f.x = max.x 
+  f.y = max.h / 2
+  f.w = max.w  / 2
+  f.h = max.h / 2
+  win:setFrame(f)
+  end)
+
 
   -- Full Screen
   hs.hotkey.bind(cmd_ctrl, "Up", function()
