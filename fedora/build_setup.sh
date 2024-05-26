9c6e3e6b18db9ea58c0f8eabcfcd91e3da6fe3c1
edw24 <erik.whipp@pm.me>
2024-05-26 11:56:38 -0700

    format bash / fedora

--- fedora/build_setup.sh
+++ fedora/build_setup.sh
@@ -4,16 +4,16 @@
 set -e
 
 set_pkg_mgr() {
-	if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-		sudo dnf clean all && \
-		sudo dnf makecache && \
-		sudo dnf install -y python3 python3-devel python3-pip python3-wheel gcc git
-	elif [[ "$OSTYPE" == "darwin"* ]]; then
-		# Mac OSX
-		if ! [ -x "$(command -v brew)" ]; then
-			/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-		fi
-	fi
+    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        sudo dnf clean all && \
+        sudo dnf makecache && \
+        sudo dnf install -y python3 python3-devel python3-pip python3-wheel gcc git
+        elif [[ "$OSTYPE" == "darwin"* ]]; then
+        # Mac OSX
+        if ! [ -x "$(command -v brew)" ]; then
+            /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+        fi
+    fi
 }
 GIT_HOME="$HOME/Documents/Resources/code"
 DOTFILES_DIR="$GIT_HOME/dots/fedora"
@@ -27,46 +27,46 @@ echo PKG_MGR=$PKG_MGR
 
 # Check if Ansible is installed
 if ! [ -x "$(command -v ansible)" ]; then
-	echo $($PKG_MGR = "dnf")
-	$([ "$PKG_MGR" = "dnf" ] && sudo "$PKG_MGR" install -y ansible || "$PKG_MGR" install ansible)
+    echo $($PKG_MGR = "dnf")
+    $([ "$PKG_MGR" = "dnf" ] && sudo "$PKG_MGR" install -y ansible || "$PKG_MGR" install ansible)
 fi
 
 # Generate ssh keys for working with Git
 
 if ! [[ -f "$SSH_DIR/id_rsa" ]]; then
-	mkdir -p "$SSH_DIR"
-	chmod 700 "$SSH_DIR"
-
-	ssh-keygen -b 4096 -t rsa -f "$SSH_DIR/id_rsa" -N "" -C "$USER@$HOSTNAME"
-	cat "$SSH_DIR/id_rsa.pub" >> "$SSH_DIR/authorized_keys"
-	chmod 600 "$SSH_DIR/authorized_keys"
+    mkdir -p "$SSH_DIR"
+    chmod 700 "$SSH_DIR"
+    
+    ssh-keygen -b 4096 -t rsa -f "$SSH_DIR/id_rsa" -N "" -C "$USER@$HOSTNAME"
+    cat "$SSH_DIR/id_rsa.pub" >> "$SSH_DIR/authorized_keys"
+    chmod 600 "$SSH_DIR/authorized_keys"
 fi
 
 if ! [[ -f "$SSH_DIR/id_ed25519" ]]; then
-	mkdir -p "$SSH_DIR"
-	chmod 700 "$SSH_DIR"
-
-	ssh-keygen -t ed25519 -f "$SSH_DIR/id_ed25519" -a 100 -C "$EMAIL"
-	cat "$SSH_DIR/id_ed25519.pub" >> "$SSH_DIR/authorized_keys"
-	chmod 600 "$SSH_DIR/authorized_keys"
+    mkdir -p "$SSH_DIR"
+    chmod 700 "$SSH_DIR"
+    
+    ssh-keygen -t ed25519 -f "$SSH_DIR/id_ed25519" -a 100 -C "$EMAIL"
+    cat "$SSH_DIR/id_ed25519.pub" >> "$SSH_DIR/authorized_keys"
+    chmod 600 "$SSH_DIR/authorized_keys"
 fi
 
 if ! [[ -d "$DOTFILES_DIR" ]]; then
-  git clone "https://github.com/ewhipp/dots.git" "$GIT_HOME"
-  cd "$GIT_HOME/dots"
+    git clone "https://github.com/ewhipp/dots.git" "$GIT_HOME"
+    cd "$GIT_HOME/dots"
 else
-  cd "$GIT_HOME/dots" && \
-  git -C "$GIT_HOME/dots" pull
+    cd "$GIT_HOME/dots" && \
+    git -C "$GIT_HOME/dots" pull
 fi
 
 if [[ -f "$DOTFILES_DIR/requirements.yml" ]]; then
-	cd "$DOTFILES_DIR"
-
-	ansible-galaxy install -r requirements.yml
+    cd "$DOTFILES_DIR"
+    
+    ansible-galaxy install -r requirements.yml
 fi
 
 if ! [[ -f "$DOTFILES_DIR/.ansible/vault.yml" ]]; then
-	ansible-vault create $DOTFILES_DIR/.ansible/vault.yml
+    ansible-vault create $DOTFILES_DIR/.ansible/vault.yml
 fi
 
 cd "$DOTFILES_DIR"
--- fedora/nvidia-fan-management.sh
+++ fedora/nvidia-fan-management.sh
@@ -1,27 +1,27 @@
 #!/bin/env bash
 
 setGpuFan() {
-	echo "*:1[gpu:0]/GPUFanControlState=1" -a "*:1[fan-0]/GPUTargetFanSpeed=$1"
-	sudo /usr/bin/nvidia-settings -a "*:1[gpu:0]/GPUFanControlState=1" -a "*:1[fan-0]/GPUTargetFanSpeed=$1"
+    echo "*:1[gpu:0]/GPUFanControlState=1" -a "*:1[fan-0]/GPUTargetFanSpeed=$1"
+    sudo /usr/bin/nvidia-settings -a "*:1[gpu:0]/GPUFanControlState=1" -a "*:1[fan-0]/GPUTargetFanSpeed=$1"
 }
 
 for (( ; ; ))
 do
-	current_gpu_temp=$(nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader)
-
-	if (( 0 <= current_gpu_temp && current_gpu_temp <= 35 ))
-	then
-		setGpuFan 40
-	elif (( 36 <= current_gpu_temp && current_gpu_temp <= 50 ))
-	then
-		setGpuFan 75
-	elif ((51<=current_gpu_temp && current_gpu_temp<=63))
-	then
-  		setGpuFan 90
-	elif ((64<=current_gpu_temp && current_gpu_temp<=70))
-	then
-		setGpuFan 100
-	fi
-
-	sleep 10
+    current_gpu_temp=$(nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader)
+    
+    if (( 0 <= current_gpu_temp && current_gpu_temp <= 35 ))
+    then
+        setGpuFan 40
+    elif (( 36 <= current_gpu_temp && current_gpu_temp <= 50 ))
+    then
+        setGpuFan 75
+    elif ((51<=current_gpu_temp && current_gpu_temp<=63))
+    then
+        setGpuFan 90
+    elif ((64<=current_gpu_temp && current_gpu_temp<=70))
+    then
+        setGpuFan 100
+    fi
+    
+    sleep 10
 done
